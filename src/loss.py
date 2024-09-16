632eebd18d0d56b700462605d37632c9c8dfcdca
jung0228 <junghw3333@gmail.com>
2024-09-16 12:07:09 +0900

    Contrastive Loss Test

--- src/loss.py
+++ src/loss.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class Loss(nn.Module):
     """
@@ -17,3 +17,17 @@ class Loss(nn.Module):
     ) -> torch.Tensor:
     
         return self.loss_fn(outputs, targets)
+    
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, output1, output2, targets):
+        # Calculate the Euclidean distance
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        # Contrastive loss formula
+        loss = torch.mean(torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        
+        return loss + self.loss_fn(output1, targets)
